882462343d32d5db56eec467fb8965b2248500c0
joaocerri <joaocerri06@gmail.com>
2026-07-22 19:11:38 -0300

    feat(app): adicionando input de dados
    feat(app): adicionando input de skills e exclude_words
    
    2026-07-22

--- app.py
+++ app.py
@@ -1,33 +1,56 @@
 import requests as r
 from datetime import datetime
 
-skills =  ["python", "machine learning", "data analysis", "sql"]
+skills =  []
+exclude_words = []
+answer = ""
+
+while answer != "0" and answer != "1":
+    answer = input("Type the skill you want to search or 0 to exit: ")
+    if(answer != "0" and answer != "1"):
+        skills.append(answer)
+
+answer = ""
+
+while answer != "0":
+    answer = input("Type the word you want to exclude or 0 to exit: ")
+    if(answer != "0" and answer != "1"):
+        exclude_words.append(answer)
 
 current_date = datetime.now().date()
 
-url = "https://api.example.com/data"
+url = "https://apis.codante.io/api/job-board/jobs"
 
 try:
     response = r.get(url)
     data = response.json()
 
-    for job in data['jobs']:
+    for job in data.get('data'):
 
-        job_title = job['title']
-        job_description = job['description']
-        finded_skills = []
-        date = datetime.strptime(job['date_posted'], "%Y-%m-%d").date()
-        totDays = (current_date - date).days
+        job_title = job.get('title')
+        job_description = job.get('description')
+        found_skills = []
+        date = datetime.strptime(job.get('updated_at')[:10], "%Y-%m-%d").date()
+        total_days = (current_date - date).days
+        has_exclude_word = False
 
-        if totDays <= 30:
+        for word in exclude_words:
+            if word.lower() in job_description.lower() or word.lower() in job_title.lower():
+                has_exclude_word = True
+
+        if has_exclude_word:
+            continue 
+
+        if total_days <= 30:
             for skill in skills:
                 if skill.lower() in job_description.lower() or skill.lower() in job_title.lower():
-                    finded_skills.append(skill)
+                    found_skills.append(skill)
 
-        if len(finded_skills) > 0:
+        if len(found_skills) > 0:
             print(f"Job Title: {job_title}")
+            print(f"Company: {job.get('company')}")
             print(f"Description: {job_description}")
-            print(f"Skills that match: {finded_skills}")
+            print(f"Skills that match: {found_skills}")
             print(f"Date Posted: {date}")
             print("-" * 40)
 except Exception as e:

bc6067da261f579ff26ff69688c5479a7b1ec564
joaocerri <joaocerri06@gmail.com>
2026-07-29 17:37:03 -0300

    feat(script): add script SQL
    2026-07-29
    
    feat(script): add script SQL

---
+++ script.sql
@@ -0,0 +1,29 @@
+CREATE TABLE
+    usuarios (
+        id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        username VARCHAR(150) NOT NULL
+    );
+
+CREATE TABLE
+    vagas (
+        id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        title VARCHAR(150) NOT NULL,
+        jobdescription TEXT NOT NULL,
+        company VARCHAR(100) NOT NULL,
+        update_at TIMESTAMP
+    );
+
+CREATE TABLE
+    skills (
+        id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+        nome VARCHAR(100) UNIQUE NOT NULL
+    );
+
+CREATE TABLE
+    vaga_skills (
+        vaga_id INTEGER NOT NULL,
+        skill_id INTEGER NOT NULL,
+        PRIMARY KEY (vaga_id, skill_id),
+        FOREIGN KEY (vaga_id) REFERENCES vagas (id) ON DELETE CASCADE,
+        FOREIGN KEY (skill_id) REFERENCES skills (id) ON DELETE CASCADE
+    );

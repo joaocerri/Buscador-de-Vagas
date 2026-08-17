396085a381337abbc023518c210b61173b69a63a
joaocerri <joaocerri06@gmail.com>
2026-08-16 22:32:35 -0300

    feat: adiciona vaga no banco
    2026-08-16
    
    feat: adiciona vaga no banco

--- job.py
+++ job.py
@@ -1,5 +1,6 @@
 import datetime
 from datetime import datetime
+import connection
 
 
 class Job:
@@ -25,3 +26,12 @@ class Job:
             if skill.lower() in self.description.lower() or skill.lower() in self.title.lower():
                 found_skills.append(skill)
         return found_skills
+
+    def createJob(self, title, description, updated_at, company, skills ):
+        try:
+            con = Connection("localhost", "buscador_vagas", "postgres", "181006") 
+            con = manipular(f"INSERT INTO jobs (title, jobdescription, updated_at, company) VALUES ('{title}', '{description}', '{updated_at}', '{company}')")
+            con.fechar()
+        except Exception as e:
+                print(f"An error occurred: {e}")
+        
